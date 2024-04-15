f6ac7874d1bc7c81c0f136aaf3cdbf01f7bd32af
RenfrewA <renfrew888@gmail.com>
2024-04-15 08:37:39 -0400

    edit data_cleaning.R

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,5 +1,5 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
+# Purpose: Cleans the raw CES2020 survey data and removing non-responses
 # Author: Renfrew Ao-Ieong
 # Date: 11 April 2024
 # Contact: renfrew.aoieong@mail.utoronto.ca
@@ -42,7 +42,99 @@ cleaned_ces2020_env <-
 
 cleaned_ces2020_env <-
   cleaned_ces2020_env |>
-  filter(!is.na(CC20_333b), ideo5 != 6, faminc_new != 97, urbancity != 5) # Filter out people who didn't answer the env question, ideo5 6: Not sure, faminc_new: 97 prefer not to say, urbancity: 5 other
+  filter(!is.na(CC20_333b), ideo5 != 6, faminc_new != 97, urbancity !=5, !is.na(urbancity)) |> # Filter out people who didn't answer the env question, ideo5 6: Not sure, faminc_new: 97 prefer not to say, urbancity: 5 other
+  mutate(
+    env_stance = if_else(CC20_333b == 1, "Support", "Oppose"),
+    env_stance = as_factor(env_stance),
+    political_pref = case_when(
+      ideo5 == 1 ~ "Very Liberal",
+      ideo5 == 2 ~ "Liberal",
+      ideo5 == 3 ~ "Moderate",
+      ideo5 == 4 ~ "Conservative",
+      ideo5 == 5 ~ "Very Convservative",
+    ),
+    household_income = factor(faminc_new,
+                              levels = c(
+                                "Very Liberal",
+                                "Liberal",
+                                "Moderate",
+                                "Conservative",
+                                "Very Conservative"
+                              )),
+    household_income = case_when(
+      faminc_new == 1 ~ "Less than $10,000",
+      faminc_new == 2 ~ "$10,000 - $19,999",
+      faminc_new == 3 ~ "$20,000 - $29,999",
+      faminc_new == 4 ~ "$30,000 - $39,999",
+      faminc_new == 5 ~ "$40,000 - $49,999",
+      faminc_new == 6 ~ "$50,000 - $59,999",
+      faminc_new == 7 ~ "$60,000 - $69,999",
+      faminc_new == 8 ~ "$70,000 - $79,999",
+      faminc_new == 9 ~ "$80,000 - $99,999",
+      faminc_new == 10 ~ "$100,000 - $119,999",
+      faminc_new == 11 ~ "$120,000 - $149,999",
+      faminc_new == 12 ~ "$150,000 - $199,999",
+      faminc_new == 13 ~ "$200,000 - $249,999",
+      faminc_new == 14 ~ "$250,000 - $349,999",
+      faminc_new == 15 ~ "$350,000 - $499,999",
+      faminc_new == 16 ~ "$500,000 or more"
+    ),
+    household_income = factor(household_income,
+                              levels = c(
+                                "Less than $10,000",
+                                "$10,000 - $19,999",
+                                "$20,000 - $29,999",
+                                "$30,000 - $39,999",
+                                "$40,000 - $49,999",
+                                "$50,000 - $59,999",
+                                "$60,000 - $69,999",
+                                "$70,000 - $79,999",
+                                "$80,000 - $99,999",
+                                "$100,000 - $119,999",
+                                "$120,000 - $149,999",
+                                "$150,000 - $199,999",
+                                "$200,000 - $249,999",
+                                "$250,000 - $349,999",
+                                "$350,000 - $499,999",
+                                "$500,000 or more"
+                              )),
+    education = case_when(
+      educ == 1 ~ "No HS",
+      educ == 2 ~ "High school graduate",
+      educ == 3 ~ "Some college",
+      educ == 4 ~ "2-year",
+      educ == 5 ~ "4-year",
+      educ == 6 ~ "Post-grad"
+    ),
+    education = factor(
+      education,
+      levels = c(
+        "No HS",
+        "High school graduate",
+        "Some college",
+        "2-year",
+        "4-year",
+        "Post-grad"
+      )
+    ),
+    living_area = case_when(
+      urbancity == 1 ~ "City",
+      urbancity == 2 ~ "Suburb",
+      urbancity == 3 ~ "Town",
+      urbancity == 4 ~ "Rural"
+    ),
+    living_area = factor(
+      living_area,
+      levels = c(
+        "City",
+        "Suburb",
+        "Town",
+        "Rural"
+      )
+    )
+  ) |>
+  select(env_stance, political_pref,household_income, education, living_area)
 
+cleaned_ces2020_env
 #### Save data ####
-write_csv(cleaned_ces2020_env, "data/analysis_data/cleaned_ces2020_env.parquet")
+write_parquet(cleaned_ces2020_env, "data/analysis_data/cleaned_ces2020_env.parquet")
