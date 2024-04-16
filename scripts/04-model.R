688623838ceb32553b86b7be918b04e36d93a164
RenfrewA <renfrew888@gmail.com>
2024-04-15 23:04:49 -0400

    edit paper and model

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -22,7 +22,7 @@ cleaned_ces2020_env <- read_parquet("data/analysis_data/cleaned_ces2020_env.parq
 # Sample 3000 observations from the cleaned dataset
 ces2020_env_reduced <- 
   cleaned_ces2020_env |>
-  slice_sample(n=3000)
+  slice_sample(n=10000)
 
 env_support <-
   stan_glm(
@@ -35,10 +35,48 @@ env_support <-
     seed = 123
   )
 
+env_support_household_income <-
+  stan_glm(
+    env_stance ~  household_income,
+    data = ces2020_env_reduced,
+    family = binomial(link = "logit"),
+    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_aux = exponential(rate = 1, autoscale = TRUE),
+    seed = 123
+  )
+
+env_support_education <-
+  stan_glm(
+    env_stance ~  education,
+    data = ces2020_env_reduced,
+    family = binomial(link = "logit"),
+    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_aux = exponential(rate = 1, autoscale = TRUE),
+    seed = 123
+  )
+
+env_support_living_area <-
+  stan_glm(
+    env_stance ~  living_area,
+    data = ces2020_env_reduced,
+    family = binomial(link = "logit"),
+    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_aux = exponential(rate = 1, autoscale = TRUE),
+    seed = 123
+  )
+
 #### Save model ####
 saveRDS(
   env_support,
   file = "models/env_support.rds"
+)
+
+saveRDS(
+  env_support_education,
+  file = "models/env_support_education.rds"
 )
 
 
